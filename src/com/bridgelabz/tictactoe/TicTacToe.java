dc1280dc2885060d04d14321d44c1330fab6134b
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-07 09:05:58 +0530

    [Akash]Added: empty space to board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,8 +1,18 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToe {
+	private static char[] board = new char[10];	
+	
+	public void assignEmptySpace() {
+		for(int i=1;i<10;i++) {
+			board[i] = ' ';
+		}
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Toe Game");
+		TicTacToe ticTacToe = new TicTacToe();
+		ticTacToe.assignEmptySpace();
 	}
 
 }
