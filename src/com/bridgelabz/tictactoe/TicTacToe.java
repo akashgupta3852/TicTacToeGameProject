848e393aca0a5e94528f8b1fb51350687519cf71
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-07 10:46:05 +0530

    [Akash]Added: show board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -13,12 +13,31 @@ public class TicTacToe {
 		System.out.println("Choose the letter X or O");
 		Scanner sc = new Scanner(System.in);
 		char option = sc.next().charAt(0);
+		sc.close();
 		return option;
 	}
+	
+	//UC3
+	public void showBoard() {
+		for(int rowPositon=1;rowPositon<10;rowPositon++) {
+			for(int columnPositon=1;columnPositon<=3;columnPositon++) {
+				System.out.print(board[columnPositon]+" ");
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
 		TicTacToe ticTacToe = new TicTacToe();
 		ticTacToe.assignEmptySpaceToBoard();
-		char option = ticTacToe.chooseXOrO();
+		char userInput = ticTacToe.chooseXOrO();
+		char computerInput;
+		if((userInput == 'X'))
+			computerInput = 'O';
+		else
+			computerInput = 'X';
+		
+		ticTacToe.showBoard();
 	}
 }
