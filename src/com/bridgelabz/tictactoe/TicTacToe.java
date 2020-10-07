fac85e4438a802df1bd240363c3fccaba750b68a
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-07 09:55:37 +0530

    [Akash]Refactored: removed extra line

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -3,20 +3,17 @@ import java.util.*;
 
 public class TicTacToe {
 	private static char[] board = new char[10];	
-	
 	//UC1
 	public void assignEmptySpaceToBoard() {
 		for(int position=0;position<10;position++)
 			board[position] = ' ';
 	}
-	
 	//UC2
 	public char chooseXOrO() {
 		System.out.println("Choose the letter X or O");
 		Scanner sc = new Scanner(System.in);
 		char option = sc.next().charAt(0);
 		return option;
-		
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
@@ -24,5 +21,4 @@ public class TicTacToe {
 		ticTacToe.assignEmptySpaceToBoard();
 		char option = ticTacToe.chooseXOrO();
 	}
-
 }
