cc1c42b2752760dcfe68ac8a682146091775be33
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-07 09:53:30 +0530

    [Akash]Added: chose option X or O

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,18 +1,28 @@
 package com.bridgelabz.tictactoe;
+import java.util.*;
 
 public class TicTacToe {
 	private static char[] board = new char[10];	
 	
-	public void assignEmptySpace() {
-		for(int i=1;i<10;i++) {
-			board[i] = ' ';
-		}
+	//UC1
+	public void assignEmptySpaceToBoard() {
+		for(int position=0;position<10;position++)
+			board[position] = ' ';
+	}
+	
+	//UC2
+	public char chooseXOrO() {
+		System.out.println("Choose the letter X or O");
+		Scanner sc = new Scanner(System.in);
+		char option = sc.next().charAt(0);
+		return option;
 		
 	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Toe Game");
+		System.out.println("Welcome to Tic Tac Toe Game");
 		TicTacToe ticTacToe = new TicTacToe();
-		ticTacToe.assignEmptySpace();
+		ticTacToe.assignEmptySpaceToBoard();
+		char option = ticTacToe.chooseXOrO();
 	}
 
 }
