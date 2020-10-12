c0355e8566a619b2906869b612ae1d33881cb3d0
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-12 14:11:20 +0530

    [Akash]Added: blocking the opponent'

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -7,13 +7,13 @@ public class TicTacToe {
 	private static char[] board = new char[10];
 	private static String player;
 
-	// UC1
+	// UC1 - Assigning empty space to the board
 	public void assignEmptySpaceToBoard() {
 		for (int position = 1; position < 10; position++)
 			board[position] = ' ';
 	}
 
-	// UC2
+	// UC2 - Deciding the symbols of user and computer
 	public char chooseSymbol(Scanner input) {
 		while (true) {
 			System.out.println("Choose the letter X or O");
@@ -34,7 +34,7 @@ public class TicTacToe {
 		}
 	}
 
-	// UC3
+	// UC3 - Displaying the game board
 	public void showBoard() {
 		for (int rowPositon = 1; rowPositon < 10; rowPositon = rowPositon + 3) {
 			for (int columnPositon = rowPositon; columnPositon <= rowPositon + 2; columnPositon++) {
@@ -53,7 +53,7 @@ public class TicTacToe {
 		}
 	}
 
-	// UC4
+	// UC4 - Entering position where user can move
 	public void userMove(Scanner input, char option) {
 		while (true) {
 			System.out.println("Enter the position where you want to put a letter:");
@@ -72,7 +72,7 @@ public class TicTacToe {
 		}
 	}
 
-	// UC5
+	// UC5 - Checking free space for which user can move
 	public void checkFreeSpace(Scanner input, char option) {
 
 		for (int position = 1; position < 10; position++) {
@@ -100,7 +100,7 @@ public class TicTacToe {
 		}
 	}
 
-	// UC6
+	// UC6 - Deciding the player who plays first
 	public int tossToDecideFirstPlayer() {
 		int toss = (int) Math.floor(Math.random() * 10) % 2;
 		switch (toss) {
@@ -114,7 +114,7 @@ public class TicTacToe {
 		return HEAD;
 	}
 
-	// UC7
+	// UC7 - Checking the winner
 	public boolean isWinner(char option) {
 		return (board[1] == board[2] && board[2] == board[3] && board[3] == option
 				|| board[4] == board[5] && board[5] == board[6] && board[6] == option
@@ -126,22 +126,67 @@ public class TicTacToe {
 				|| board[3] == board[6] && board[6] == board[9] && board[9] == option);
 	}
 
-	// UC8
+	// UC8 - Computer plays
 	private boolean computerMove(char option) {
 		while (true) {
-			int computerPosition = (int) Math.floor(Math.random() * 10) % 9 + 1;
-			if (board[computerPosition] == ' ') {
-				option = (option == 'X') ? 'O' : 'X';
-				board[computerPosition] = option;
-				System.out.println("Computer moved at the position: " + computerPosition);
+			int computerPosition = block(option);
+			option = (option == 'X') ? 'O' : 'X';
+			if (computerPosition != 0) {
+				if (board[computerPosition] == ' ') {
+					board[computerPosition] = option;
+					System.out.println("Computer moved at the position: " + computerPosition + " to block the user");
+					if (isWinner(option)) {
+						showBoard();
+						System.out.println("Computer is winner");
+						return true;
+					}
+				}
+			} else {
+				computerPosition = (int) Math.floor(Math.random() * 10) % 9 + 1;
+				if (board[computerPosition] == ' ')
+					board[computerPosition] = option;
+				else {
+					option = (option == 'X') ? 'O' : 'X';
+					continue;
+				}
+				System.out.println("Computer moved at the random position: " + computerPosition);
 				if (isWinner(option)) {
 					showBoard();
 					System.out.println("Computer is winner");
 					return true;
 				}
-				return false;
 			}
+			return false;
 		}
+	}
+
+	// UC9 - Blocking the opponent
+	public int block(char option) {
+		if (board[1] == board[2] && board[2] == option && board[3] ==' ')	return 3;
+		else if (board[2] == board[3] && board[2] == option && board[1] ==' ')	return 1;
+		else if (board[1] == board[3] && board[3] == option && board[2] ==' ')	return 2;
+		else if (board[4] == board[5] && board[4] == option && board[6] ==' ')	return 6;
+		else if (board[5] == board[6] && board[5] == option && board[4] ==' ')	return 4;
+		else if (board[4] == board[6] && board[4] == option && board[5] ==' ')	return 5;
+		else if (board[7] == board[8] && board[7] == option && board[9] ==' ')	return 9;
+		else if (board[8] == board[9] && board[8] == option && board[7] ==' ')	return 7;
+		else if (board[7] == board[9] && board[7] == option && board[8] ==' ')	return 8;
+		else if (board[1] == board[5] && board[1] == option && board[9] ==' ')	return 9;
+		else if (board[5] == board[9] && board[5] == option && board[1] ==' ')	return 1;
+		else if (board[1] == board[9] && board[1] == option && board[5] ==' ')	return 5;
+		else if (board[3] == board[5] && board[3] == option && board[7] ==' ')	return 7;
+		else if (board[5] == board[7] && board[5] == option && board[3] ==' ')	return 3;
+		else if (board[3] == board[7] && board[3] == option && board[5] ==' ')	return 5;
+		else if (board[1] == board[4] && board[1] == option && board[7] ==' ')	return 7;
+		else if (board[4] == board[7] && board[4] == option && board[1] ==' ')	return 1;
+		else if (board[1] == board[7] && board[1] == option && board[4] ==' ')	return 4;
+		else if (board[2] == board[5] && board[2] == option && board[8] ==' ')	return 8;
+		else if (board[5] == board[8] && board[5] == option && board[2] ==' ')	return 2;
+		else if (board[2] == board[8] && board[2] == option && board[5] ==' ')	return 5;
+		else if (board[3] == board[6] && board[3] == option && board[9] ==' ')	return 9;
+		else if (board[6] == board[9] && board[6] == option && board[3] ==' ')	return 3;
+		else if (board[3] == board[9] && board[3] == option && board[6] ==' ') return 6;
+		return 0;
 	}
 
 	public static void main(String[] args) {
